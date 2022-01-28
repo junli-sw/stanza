9d02eada822a3060f00254eaf228f69d76d868d7
junli-sw <jun.r.li@gmail.com>
2022-01-28 12:09:36 -0500

    Update __init__.py
    set up env variables for training

--- stanza/__init__.py
+++ stanza/__init__.py
@@ -4,6 +4,7 @@ from stanza.models.common.doc import Document
 from stanza.resources.common import download
 from stanza.resources.installation import install_corenlp, download_corenlp_models
 from stanza._version import __version__, __resources_version__
+import os
 
 import logging
 logger = logging.getLogger('stanza')
@@ -25,3 +26,21 @@ log_handler.setFormatter(log_formatter)
 #   logger.removeHandler(stanza.log_handler)
 if not logger.hasHandlers():
     logger.addHandler(log_handler)
+
+proj_base = os.environ[ "TRAIN_PROJ_BASE"]
+os.environ[ "UDBASE"] = f"{proj_base}/data/udbase"
+os.environ[ "NERBASE" ] = f"{proj_base}/data/nerbase"
+os.environ["DATA_ROOT"] = f"{proj_base}/data/processed"
+os.environ[ "WORDVEC_DIR"] = f"{proj_base}/data/wordvec"
+
+data_root = os.environ[ "DATA_ROOT"]
+os.environ[ "TOKENIZE_DATA_DIR"] = f"{data_root}/tokenize"
+os.environ["MWT_DATA_DIR"] = f"{data_root}/mwt"
+os.environ["LEMMA_DATA_DIR"] = f"{data_root}/lemma"
+os.environ["POS_DATA_DIR"] = f"{data_root}/pos"
+os.environ[ "DEPPARSE_DATA_DIR"] = f"{data_root}/depparse"
+os.environ[ "ETE_DATA_DIR"] = f"{data_root}/ete"
+
+os.environ["NER_DATA_DIR"] = f"{data_root}/ner"
+os.environ["CHARLM_DATA_DIR"] = f"{data_root}/charlm"
+
